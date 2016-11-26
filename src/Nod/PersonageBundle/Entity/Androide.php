e649dee85e67eb6baafbb5f5012210c43c0113eb
dev <dev@debian>
2016-11-26 17:11:03 +0100

    Ajout Entity Androide variable hardware et software 261120161700

---
+++ src/Nod/PersonageBundle/Entity/Androide.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Androide
+ *
+ * @author dev
+ */
+class Androide {
+    
+    /*
+     * hardware 
+    */
+
+    protected $force;
+    
+    protected $agilite;
+    
+    protected $velocite;
+    
+    protected $structure;
+    
+    /*
+     * software
+     */
+    
+    protected $puissance;
+    
+    protected $interface;
+
+    protected $memoire;
+    
+    protected $circuit;
+    
+    
+    
+    
+    
+    
+}
